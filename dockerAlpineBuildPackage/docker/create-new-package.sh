1a81cbd6cd09e008ba4f41da2005194ba8d2192e
ulfhaga <ulf.haga@gmail.com>
2020-06-04 21:22:16 +0200

    Saving

--- dockerAlpineBuildPackage/docker/create-new-package.sh
+++ dockerAlpineBuildPackage/docker/create-new-package.sh
@@ -1,8 +1,38 @@
 #!/bin/sh
-read -p "Enter the new AKP package name : " package_name
-# Create package
-./run-docker
 
-docker exec -t -w /home/dev/aports   apk-build   newapkbuild -c ${package_name}
+read -r -p "Enter the new AKP package name : " package_name
+# Create package
+./stop.sh
+./run-docker
+pkg_desc="Mydescription"
+docker exec -t -w /home/dev/aports apk-build newapkbuild ${package_name}
+
+# Add tar ball
+tarball=software-1.0.tar;
+tar -cvf ${tarball} -C ../source hello.sh
+
+ # Update file /etc/abuild.conf
+ set -x
+sed -i -e "s/source=\"\"/source=\""${tarball}"\"/" ../aports/"${package_name}"/APKBUILD
+
+pkg_version=1.0
+sed -i -e "s/pkgver=\"\"/pkgver=\""${pkg_version}"\"/" ../aports/"${package_name}"/APKBUILD
+
+pkg_desc="Mydescription"
+sed -i -e "s/pkgdesc=\"\"/pkgdesc=\""${pkg_desc}"\"/"  ../aports/"${package_name}"/APKBUILD
+
+url="www.github.com"
+sed -i -e "s/url=\"\"/url=\""${url}"\"/"  ../aports/"${package_name}"/APKBUILD
+
+license="LGPL-2.1-or-later";
+sed -i -e "s/license=\"\"/license=\""${license}"\"/"  ../aports/"${package_name}"/APKBUILD
+
+sed -i -e "s/license=\"\"/license=\""${license}"\"/"  ../aports/"${package_name}"/APKBUILD
+
+cp ${tarball} ../aports/${package_name}
+
+mkdir -p ../aports/"${package_name}"/pkg/test
+mkdir -p ../aports/"${package_name}"/pkg/test-dev
+
 ./stop.sh
 
--- dockerAlpineBuildPackage/readme.txt
+++ dockerAlpineBuildPackage/readme.txt
@@ -4,3 +4,8 @@ https://wiki.alpinelinux.org/wiki/Creating_an_Alpine_package
 
 https://www.erianna.com/creating-a-alpine-linux-repository/
 
+https://wiki.alpinelinux.org/wiki/Abuild_and_Helpers#newapkbuild
+
+
+Docker install
+https://docs.docker.com/engine/install/
