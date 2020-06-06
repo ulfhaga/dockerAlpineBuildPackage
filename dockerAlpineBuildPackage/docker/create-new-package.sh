9d852649a8acf2a6759f3aa8ba9fa1f9e16b9990
ulfhaga <ulf.haga@gmail.com>
2020-06-06 19:46:21 +0200

    Saving

--- dockerAlpineBuildPackage/docker/create-new-package.sh
+++ dockerAlpineBuildPackage/docker/create-new-package.sh
@@ -7,32 +7,45 @@ read -r -p "Enter the new AKP package name : " package_name
 pkg_desc="Mydescription"
 docker exec -t -w /home/dev/aports apk-build newapkbuild ${package_name}
 
-# Add tar ball
-tarball=software-1.0.tar;
-tar -cvf ${tarball} -C ../source hello.sh
 
- # Update file /etc/abuild.conf
- set -x
-sed -i -e "s/source=\"\"/source=\""${tarball}"\"/" ../aports/"${package_name}"/APKBUILD
+# Update file /etc/abuild.conf
+
+set -x
+
+# Update file APKBUILD
 
 pkg_version=1.0
 sed -i -e "s/pkgver=\"\"/pkgver=\""${pkg_version}"\"/" ../aports/"${package_name}"/APKBUILD
 
-pkg_desc="Mydescription"
-sed -i -e "s/pkgdesc=\"\"/pkgdesc=\""${pkg_desc}"\"/"  ../aports/"${package_name}"/APKBUILD
+sed -i -e 's/source=\"\"/source=\"$pkgname-$pkgver.tar.gz\"/' ../aports/"${package_name}"/APKBUILD
 
-url="www.github.com"
-sed -i -e "s/url=\"\"/url=\""${url}"\"/"  ../aports/"${package_name}"/APKBUILD
+APKBUILD_FILE="../aports/"${package_name}"/APKBUILD"
+
+pkg_desc="Mydescription"
+sed -i -e "s/pkgdesc=\".*\"/pkgdesc=\""${pkg_desc}"\"/"  ../aports/"${package_name}"/APKBUILD
+
+url="http:\/\/www.github.com"
+sed -i -e "s/url=\".*\"/url=\""${url}"\"/"  ../aports/"${package_name}"/APKBUILD
 
 license="LGPL-2.1-or-later";
-sed -i -e "s/license=\"\"/license=\""${license}"\"/"  ../aports/"${package_name}"/APKBUILD
+sed -i -e "s/license=\".*\"/license=\""${license}"\"/"  ../aports/"${package_name}"/APKBUILD
 
-sed -i -e "s/license=\"\"/license=\""${license}"\"/"  ../aports/"${package_name}"/APKBUILD
+sed -i -e "s/license=\".*\"/license=\""${license}"\"/"  ../aports/"${package_name}"/APKBUILD
 
+sed -i -e "s/subpackages=\".*\"/subpackages=\"\"/"  "${APKBUILD_FILE}"
+
+# function_package="cd \"$builddir\"; mkdir -p $pkgdir;";
+
+sed -i -e "s/^package.*/&\n cd \"\$builddir\"; mkdir -p \$pkgdir;/" "${APKBUILD_FILE}"
+
+
+# Add tar ball
+tarball=test-1.0.tar.gz;
+tar -cvzf ${tarball} -C ../source test-1.0
 cp ${tarball} ../aports/${package_name}
 
-mkdir -p ../aports/"${package_name}"/pkg/test
-mkdir -p ../aports/"${package_name}"/pkg/test-dev
+# mkdir -p ../aports/"${package_name}"/pkg/test
+# mkdir -p ../aports/"${package_name}"/pkg/test-dev
 
 ./stop.sh
 
